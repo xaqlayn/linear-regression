59b0609ca0ac9e32660e740a2ed0ae6eec35561c
xaqlain <148036120+xaqlayn@users.noreply.github.com>
2025-07-12 15:00:55 +0200

    Create init.py

---
+++ init.py
@@ -0,0 +1,9 @@
+from .models import LinearRegressionNormal, LinearRegressionGD
+from .utils import mean_squared_error, r2_score
+
+__all__ = [
+    'LinearRegressionNormal',
+    'LinearRegressionGD',
+    'mean_squared_error',
+    'r2_score'
+]
